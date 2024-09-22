288d57f3280b31f78a9a03a801f81942e8c0e5c4
Caique <caiquegomesdev@gmail.com>
2024-09-21 23:04:10 -0300

    Aula03(TiposPrimitivosExercicio)

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    //psvm
+    public static void main(String[] args) {
+        String nome = "Caique Gomes";
+        int endereco = 242;
+        double salario = 3.500;
+        String data = "15/10/2025";
+        System.out.println("Eu " + nome + ", morando no " + endereco + ", confirmo que recebi o salario de " + salario+ ", na data " + data);
+    }
+}
