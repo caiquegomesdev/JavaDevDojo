5a9fd2ea4f5ddbf3a00c4bc3dd12944b771d8bd5
Caique <caiquegomesdev@gmail.com>
2024-09-28 23:58:54 -0300

    Aula06 - Estruturas de Repeticao Exercicio 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula06EstruturasRepeticaoExercicio01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasRepeticaoExercicio01 {
+    public class Cavalos {
+        public static void main(String[] args) {
+            double m, h, t, cavalos;
+            m = 21;
+            h = 2.2;
+            t = 1.4;
+
+            cavalos = (m * h / t) / 745.6999;
+
+            System.out.println("A quantidade de cavalos necessários é " + cavalos);
+        }
+    }
+}
