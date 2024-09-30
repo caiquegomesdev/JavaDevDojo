030c67356979d9e23371de296993d9307ae4b4dd
Caique <caiquegomesdev@gmail.com>
2024-09-29 23:48:00 -0300

    Aula06 - Estruturas de Repeticao 02

--- maratona-java/src/academy/devdojo/maratonajava/introducao/Aula06EstruturasRepeticao02.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula06EstruturasRepeticao02.java
@@ -3,8 +3,9 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula06EstruturasRepeticao02 {
     public static void main(String[] args) {
         for (int i = 0; i < 1000000 ; i++) {
-            
-            System.out.println(i);
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
