f011d5327596648c99ad024b38c2f6f07f9ce6fe
Caique <caiquegomesdev@gmail.com>
2024-09-30 21:50:41 -0300

    Aula06 - Estruturas de Repeticao 04

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula06EstruturasRepeticao04.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasRepeticao04 {
+    public static void main(String[] args) {
+        double valorTotal = 30000;
+        for (int parcela = 0; parcela <= valorTotal ; parcela++) {
+            double valorParcela = valorTotal / parcela;
+//            if (valorParcela < 1000){
+//                break;
+//            }
+            if(valorParcela >= 1000){
+                System.out.println("Parcela " + parcela + "R$ " + valorParcela);
+            } else {
+                break;
+            }
+        }
+    }
+}
