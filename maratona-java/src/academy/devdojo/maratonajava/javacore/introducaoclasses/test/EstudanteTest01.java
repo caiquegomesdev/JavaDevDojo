03de1bf0d1e3b7252de5dae27731975307058e74
Caique <caiquegomesdev@gmail.com>
2024-10-06 21:48:07 -0300

    Aula09 - Orientação Objeto - Introdução Classes 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -4,6 +4,13 @@ import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante
 
 public class EstudanteTest01 {
     public static void main(String[] args) {
-        Estudante estudante;
+        Estudante estudante = new Estudante();
+        estudante.nome = "Luffy";
+        estudante.idade = 21;
+        estudante.sexo = 'M';
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante);
     }
 }
