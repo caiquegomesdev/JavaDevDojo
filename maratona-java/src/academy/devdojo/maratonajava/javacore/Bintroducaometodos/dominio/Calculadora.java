da45c38d0c7d9b380aa077423ce35f1b99ff910b
Caique <caiquegomesdev@gmail.com>
2024-10-11 00:24:11 -0300

    Aula09 - Orientação Objetos - Metodos 02 - MinhaVersao

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -1,11 +1,49 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
+import java.util.Scanner;
+
 public class Calculadora {
+
     public void somaDoisNumeros(){
-        System.out.println(10+10);
+        Scanner sc = new Scanner(System.in);
+        int soma = sc.nextInt();
+        int soma2 = sc.nextInt();
+        System.out.println("Soma: ");
+        System.out.println(soma + soma2);
+
+
     }
 
     public void subtraiDoisNumeros(){
-        System.out.println(20-1);
+        Scanner sc = new Scanner(System.in);
+        int subtracao = sc.nextInt();
+        int subtracao2 = sc.nextInt();
+
+        System.out.println("Subtracao: ");
+        System.out.println(subtracao - subtracao2);
+
     }
+
+    public void multiplicaDoisNumeros(){
+        Scanner sc = new Scanner(System.in);
+        int multiplica = sc.nextInt();
+        int multiplica2 = sc.nextInt();
+
+        System.out.println("Multiplicacao: ");
+        System.out.println(multiplica * multiplica2);
+
+    }
+
+    public void divideDoisNumeros(){
+        Scanner sc = new Scanner(System.in);
+        int divide = sc.nextInt();
+        int divide2 = sc.nextInt();
+
+        System.out.println("Divisao: ");
+        System.out.println(divide / divide2);
+        sc.close();
+    }
+
+
+
 }
--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -2,11 +2,17 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
 
+import java.util.Scanner;
+
 public class CalculadoraTest01 {
     public static void main(String[] args) {
+
         Calculadora calculadora = new Calculadora();
         calculadora.somaDoisNumeros();
         calculadora.subtraiDoisNumeros();
+        calculadora.multiplicaDoisNumeros();
+        calculadora.divideDoisNumeros();
+
         System.out.println("Finalizando CalculadoraTest01");
     }
 
