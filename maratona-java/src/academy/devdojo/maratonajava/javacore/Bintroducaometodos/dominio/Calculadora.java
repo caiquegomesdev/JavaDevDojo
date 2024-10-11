99564086ad7307c606a21c3caa260a937ce11577
Caique <caiquegomesdev@gmail.com>
2024-10-11 00:02:49 -0300

    Aula09 - Orientação Objetos - Metodos 02 - Parâmetros

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -4,4 +4,8 @@ public class Calculadora {
     public void somaDoisNumeros(){
         System.out.println(10+10);
     }
+
+    public void subtraiDoisNumeros(){
+        System.out.println(20-1);
+    }
 }
