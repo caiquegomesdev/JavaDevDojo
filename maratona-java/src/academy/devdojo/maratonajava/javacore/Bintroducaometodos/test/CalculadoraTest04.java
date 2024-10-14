d5b09eb3e7a9b7f412d6cdda70002574d757b2a6
Caique <caiquegomesdev@gmail.com>
2024-10-14 00:01:00 -0300

    Aula09 - Orientação Objetos - Métodos pt 05

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest04.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest04.java
@@ -8,8 +8,8 @@ public class CalculadoraTest04 {
 
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        int num1 = 1;
-        int num2 = 2;
+        int num1 = 10;
+        int num2 = 20;
 
         calculadora.alteraDoisNumeros(num1, num2);
         System.out.println(num1);
