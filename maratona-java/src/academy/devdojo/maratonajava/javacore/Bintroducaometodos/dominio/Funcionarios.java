fe5c8fa8dd18c0948ba8476d7a2acbe713f9095c
Caique <caiquegomesdev@gmail.com>
2024-10-16 22:58:56 -0300

    Aula09 - Orientação Objetos - Métodos pt 10 - Exercise

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionarios.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Funcionarios {
+    public String nome;
+    public int idade;
+    public double[] salarios;
+
+    public void imprimeDados(){
+
+        System.out.println("-----------------");
+        System.out.println("NOME: " + this.nome);
+        System.out.println("IDADE: " + this.idade);
+        System.out.println("SALARIO 1: " + this.salarios[0]);
+        System.out.println("SALARIO 2: " +this.salarios[1]);
+        System.out.println("SALARIO 3: " +this.salarios[2]);
+        for (double salario: salarios){
+            System.out.print(salario + " ");
+        }
+    }
+
+    public void imprimeMediaSalario(){
+        double media = 0;
+        for (double salario: salarios){
+            media =+ salario;
+        }
+        media /= salarios.length;
+        System.out.println("\nMEDIA SALARIAL: " + media);
+    }
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionariosTest.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Funcionarios;
+
+public class FuncionariosTest {
+    public static void main(String... args) {
+        Funcionarios funcionario = new Funcionarios();
+
+
+        funcionario.nome= "Gomes";
+        funcionario.idade = 25;
+        funcionario.salarios = new double[]{1200, 2000.20, 5500.99};
+
+        funcionario.imprimeDados();
+        funcionario.imprimeMediaSalario();
+    }
+}
