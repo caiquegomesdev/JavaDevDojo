ec60b732c0c0d3c1ad8bafe6bae32b37b99957eb
Caique <caiquegomesdev@gmail.com>
2024-10-17 23:04:00 -0300

    Aula09 - Orientação Objetos - Modificador de acesso private, get e set pt 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Pessoa.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Pessoa {
+    public String nome;
+    public int idade;
+
+    public  void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+    }
+}
