7ffc5edbb7ec9f9ca96836a6a086424ad938e721
Caique <caiquegomesdev@gmail.com>
2024-10-21 00:20:07 -0300

    Aula11 - Orientação Objetos - Construtores pt 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.test;
+
+
+import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("JujutsuKaisen");
+        anime.init("Boku no hero","TV", 20);
+        anime.init("Boku no hero","TV", 20,"Ação");
+        anime.imprime();
+//sobrecarga é metodos com o mesmo nome com quantidades ou tipos dos parametros diferentes
+    }
+}
