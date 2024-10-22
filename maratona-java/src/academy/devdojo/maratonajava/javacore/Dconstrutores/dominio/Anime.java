1c68fc14e521aa93214901829fee829566b2d1e1
Caique <caiquegomesdev@gmail.com>
2024-10-22 00:10:50 -0300

    Aula59 - Orientação Objetos - Construtores pt 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -1,30 +1,35 @@
 package academy.devdojo.maratonajava.javacore.Dconstrutores.dominio;
 
+import javax.swing.*;
+
 public class Anime {
     private String nome;
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
+    //      sobrecarga é metodos com o mesmo nome com quantidades ou tipos dos parametros diferentes
     public Anime(String nome, String tipo, int episodios, String genero){
-        System.out.println("DENTRO DO CONSTRUTOR: ");
+        System.out.println("--DENTRO DO CONSTRUTOR:-- ");
+
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
     }
 
-    public Anime(){
-//      sobrecarga é metodos com o mesmo nome com quantidades ou tipos dos parametros diferentes
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio){
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
     }
 
-
     public void imprime(){
-        System.out.println(this.nome);
-        System.out.println(this.tipo);
-        System.out.println(this.episodios);
-        System.out.println(this.genero);
-
+        System.out.println("NOME: " + this.nome);
+        System.out.println("TIPO: " + this.tipo);
+        System.out.println("EPISODIOS: " + this.episodios);
+        System.out.println("GENERO: " + this.genero);
+        System.out.println("ESTUDIO: " + this.estudio);
     }
 
     public String getNome() {
--- maratona-java/src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -5,10 +5,10 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("JujutsuKaisen","TV", 20,"Ação");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("JujutsuKaisen","TV", 20,"Ação", "MAPPA");
+
         anime.imprime();
-        anime2.imprime();
+
 
     }
 }
