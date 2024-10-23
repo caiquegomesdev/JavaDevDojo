febb4a462017ebea922a1de2413ed97f27449935
Caique <caiquegomesdev@gmail.com>
2024-10-22 23:49:42 -0300

    Aula60 - Orientação Objetos - Modificador static

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Carro.java
@@ -0,0 +1,45 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio;
+
+public class Carro {
+    private String nome;
+    private double velocidadeMaxima;
+    private  double velocidadeLimite = 250;
+
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeLimite = velocidadeLimite;
+    }
+
+    public void imprime(){
+        System.out.println("-----------------------");
+        System.out.println("Nome " + this.nome);
+        System.out.println("Velocidade Maxima " + this.velocidadeMaxima);
+        System.out.println("velociade Limite " + this.velocidadeLimite);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public double getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+
+    public void setVelocidadeLimite(double velocidadeLimite) {
+        this.velocidadeLimite = velocidadeLimite;
+    }
+}
+
