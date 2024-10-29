69f624b9434ddd1fb645e6bf4deb0f422ab1d930
Caique <caiquegomesdev@gmail.com>
2024-10-28 23:00:57 -0300

    Aula68 - Orientação Objetos - Leitura de dados pelo console

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Gassociacao/Test/LeituraDoTecladoTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Gassociacao/Test/LeituraDoTecladoTest01.java
@@ -6,12 +6,20 @@ public class LeituraDoTecladoTest01 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("DIGITE SEU NOME ABAIXO: ");
-        String next = entrada.next();
-        System.out.println("Seu nome é " + next);
-        System.out.println("----------------------------");
+        String nome = entrada.next();
         System.out.println("DIGITTE SUA IDADE ABAIXO: ");
-        
+        int idade = entrada.nextInt();
+        System.out.println("DIGITE (M) OU (F) PARA SEU SEXO: ");
+        char sexo = entrada.next().charAt(0);
 
+        System.out.println("----------------------------");
+        System.out.println("Seu nome é " + nome + " sua idade é " + idade + " anos, seu sexo é " + sexo);
+
+        if (Character.toLowerCase(sexo) == 'm') {
+            System.out.println("MASCULINO");
+        } else {
+            System.out.println("FEMININO");
+        }
 
         entrada.close();
     }
