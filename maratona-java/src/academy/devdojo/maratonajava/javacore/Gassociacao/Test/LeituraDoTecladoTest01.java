6ae0f59a7b2209fbe488e3a276f39d3d00a62bca
Caique <caiquegomesdev@gmail.com>
2024-10-28 22:48:28 -0300

    Aula68 - Orientação Objetos - Leitura de dados pelo console

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Gassociacao/Test/LeituraDoTecladoTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.Test;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTest01 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("DIGITE SEU NOME ABAIXO: ");
+        String next = entrada.next();
+        System.out.println("Seu nome é " + next);
+        
+
+        entrada.close();
+    }
+}
