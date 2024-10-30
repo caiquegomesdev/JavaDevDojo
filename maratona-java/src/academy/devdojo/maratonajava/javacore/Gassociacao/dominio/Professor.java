b62a4f8ad651ef842592252be47a0fcb1abd39d2
Caique <caiquegomesdev@gmail.com>
2024-10-29 22:50:18 -0300

    Aula70 - Orientação Objetos - Associação pt 05 - Exercício Associação

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Professor.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Professor.java
@@ -2,10 +2,25 @@ package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
 
 public class Professor {
     private String nome;
+    private String especialidade;
+    private Seminario[] seminarios;
 
     public Professor(String nome) {
         this.nome = nome;
     }
+
+    public Professor(String nome, String especialidade) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+    }
+
+    public Professor(String nome, String especialidade, Seminario[] seminarios) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.seminarios = seminarios;
+    }
+
+    
 
     public String getNome() {
         return nome;
@@ -14,4 +29,21 @@ public class Professor {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public Seminario[] getSeminarios() {
+        return seminarios;
+    }
+
+    public void setSeminarios(Seminario[] seminarios) {
+        this.seminarios = seminarios;
+    }
+
 }
