9241491ae258717f0ee8bbff09901db342204b56
Caique <caiquegomesdev@gmail.com>
2024-11-05 21:57:36 -0300

    Aula75 - Orientação Objetos - Herança pt 05 - Sequência de inicialização

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -4,8 +4,19 @@ public class Funcionario extends  Pessoa{
 
     private double salario;
 
+    static {
+        System.out.println("Dentro do bloco de inicialização estatico de Funcionario.");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização NÃO estatico de Funcionario.1");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização NÃO estatico de Funcionario.2");
+    }
+
     public Funcionario(String nome){
         super(nome);
+        System.out.println("Dentro do construtor de Funcionario");
     }
 
     public void imprime() {
