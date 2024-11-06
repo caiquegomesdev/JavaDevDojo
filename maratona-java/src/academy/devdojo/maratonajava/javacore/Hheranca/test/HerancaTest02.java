96ef38554fcd49bdb45de6e1be2a66bc76a8e608
Caique <caiquegomesdev@gmail.com>
2024-11-05 22:08:29 -0300

    Aula75 - Orientação Objetos - Herança pt 05 - Sequência de inicialização

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest02.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest02.java
@@ -5,10 +5,14 @@ import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
 public class HerancaTest02 {
     // 0 - Bloco de inicialização estatico da superclasse é executado quando JVM carregar a classe pai
     // 1 - Bloco de inicialização estatico da superclasse é executado quando JVM carregar a classe filha
-    // 2 - Alocado espaco em memoria pro objeto
-    // 3 - Cada atributo de classe é criado e inicializado com valores default ou o quer for passado
-    // 4 - Bloco de inicialização é executado
-    // 5 - Construtor é executado
+    // 2 - Alocado espaco em memoria pro objeto da superclasse
+    // 3 - Cada atributo de superclasse é criado e inicializado com valores default ou o quer for passado
+    // 4 - Bloco de inicialização da superclasse é executado na ordem em que aparece
+    // 5 - Construtor é executado da superclasse
+    // 6 - Alocado espaco em memoria pro objeto da subclasse
+    // 7 - Cada atributo de subclasse é criado e inicializado com valores default ou o quer for passado
+    // 8 - Bloco de inicialização da subclasse é executado na ordem em que aparece
+    // 9 - Construtor é executado da subclasse
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario("Joao");
     }
