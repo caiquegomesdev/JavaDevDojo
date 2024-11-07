4b579e7579c44aef2d9e0820b2f029dda863b778
Caique <caiquegomesdev@gmail.com>
2024-11-06 22:03:58 -0300

    Aula77 - Orientação Objetos - Modificador final pt 01 - Tipo primitivo

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Isobrescrita/dominio/Anime.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Isobrescrita/dominio/Anime.java
@@ -4,9 +4,12 @@ public class Anime extends Object{
     private String nome;
 
     @Override
-    public String toString(){
-        return "Anime: " + this.nome;
+    public String toString() {
+        return "Anime{" +
+                "nome='" + nome + '\'' +
+                '}';
     }
+
     public Anime(String nome) {
         this.nome = nome;
     }
