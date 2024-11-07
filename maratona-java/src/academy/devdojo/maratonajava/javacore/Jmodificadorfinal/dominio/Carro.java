133ea87333461049694311562d417ead6c2abd57
Caique <caiquegomesdev@gmail.com>
2024-11-06 22:14:35 -0300

    Aula78 - Orientação Objetos - Modificador final pt 02 - Tipo referência

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -3,6 +3,7 @@ package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
 public class Carro {
     private String nome;
     public static final double VELOCIDADE_LIMITE = 250;//final = constante
+    public final Comprador COMPRADOR = new Comprador();//a ref desse obj nunca poderar se alterada
 
     public String getNome() {
         return nome;
