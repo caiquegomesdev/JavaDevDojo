e28e6358ec9b29f16f1fc9caa3db9e03e7535b22
Caique <caiquegomesdev@gmail.com>
2024-11-06 22:18:16 -0300

    Aula78 - Orientação Objetos - Modificador final pt 02 - Tipo referência

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -1,11 +1,14 @@
 package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.test;
 
 import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Comprador;
 
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
         System.out.println(Carro.VELOCIDADE_LIMITE);
-        
+        System.out.println(Carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Okarun");
+        System.out.println(carro.COMPRADOR);
     }
 }
