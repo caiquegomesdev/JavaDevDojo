81dcb01df6079acf036f5fa4347ba8d9737b3e09
Caique <caiquegomesdev@gmail.com>
2024-11-10 00:03:38 -0300

    Aula80 - Orientação Objetos - Enumeração pt 01 - Introdução

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
@@ -2,25 +2,18 @@ package academy.devdojo.maratonajava.javacore.Kenum.dominio;
 
 public class Cliente {
     private String nome;
-    private String tipo;
-    public static  final String PESSOA_FISICA = "PESSOA_FISICA";
-    public static  final String PESSOA_JURIDICA = "PESSOA_JURIDICA";
+    private TipoCliente tipoCliente;
 
-
-
-    public Cliente(String nome, String tipo) {
-        if(!tipo.equals(PESSOA_FISICA) && !tipo.equals(PESSOA_JURIDICA)){
-            return;
-        }
+    public Cliente(String nome, TipoCliente tipoCliente) {
         this.nome = nome;
-        this.tipo = tipo;
+        this.tipoCliente = tipoCliente;
     }
 
     @Override
     public String toString() {
         return "Cliente{" +
                 "nome='" + nome + '\'' +
-                ", tipo='" + tipo + '\'' +
+                ", tipoCliente=" + tipoCliente +
                 '}';
     }
 
@@ -32,11 +25,11 @@ public class Cliente {
         this.nome = nome;
     }
 
-    public String getTipo() {
-        return tipo;
+    public TipoCliente getTipoCliente() {
+        return tipoCliente;
     }
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
+    public void setTipoCliente(TipoCliente tipoCliente) {
+        this.tipoCliente = tipoCliente;
     }
 }
--- maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -1,13 +1,14 @@
 package academy.devdojo.maratonajava.javacore.Kenum.test;
 
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Tsubasa","pessoa fisica");
-        Cliente cliente2 = new Cliente("Myamoto", "Pessa Juridica");
-        Cliente cliente3 = new Cliente("Shikamaru", "PESSOA_FISICA");
-        Cliente cliente4 = new Cliente("Atena", "PESSOA_JURIDICA");
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
+        Cliente cliente2 = new Cliente("Myamoto", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente3 = new Cliente("Shikamaru",TipoCliente.PESSOA_FISICA );
+        Cliente cliente4 = new Cliente("Atena", TipoCliente.PESSOA_JURIDICA);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
