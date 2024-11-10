4ee3c56358c71ebf775fc66d58ceeded2605106f
Caique <caiquegomesdev@gmail.com>
2024-11-10 20:59:49 -0300

    Aula82 - Orientação Objetos - Enumeração pt 03 - Sobrescrita de métodos

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -2,19 +2,22 @@ package academy.devdojo.maratonajava.javacore.Kenum.test;
 
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
-import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente.TipoPagamento;
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoPagamento;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
-        Cliente cliente2 = new Cliente("Myamoto", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
-        Cliente cliente3 = new Cliente("Shikamaru",TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.CREDITO );
-        Cliente cliente4 = new Cliente("Atena", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.DEBITO);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Myamoto", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+        Cliente cliente3 = new Cliente("Shikamaru",TipoCliente.PESSOA_FISICA, TipoPagamento.CREDITO );
+        Cliente cliente4 = new Cliente("Atena", TipoCliente.PESSOA_JURIDICA, TipoPagamento.DEBITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
         System.out.println(cliente3);
         System.out.println(cliente4);
+
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
 
     }
 }
