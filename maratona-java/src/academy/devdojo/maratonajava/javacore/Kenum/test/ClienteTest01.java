fcd3ab11558a778683537609fa6ab89d8f5b8ad1
Caique <caiquegomesdev@gmail.com>
2024-11-09 23:58:06 -0300

    Aula80 - Orientação Objetos - Enumeração pt 01 - Introdução

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -4,10 +4,15 @@ import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Tsubasa","PESSOA_FISICA");
-        Cliente cliente2 = new Cliente("Myamoto", "PESSOA_JURIDICA");
+        Cliente cliente1 = new Cliente("Tsubasa","pessoa fisica");
+        Cliente cliente2 = new Cliente("Myamoto", "Pessa Juridica");
         Cliente cliente3 = new Cliente("Shikamaru", "PESSOA_FISICA");
         Cliente cliente4 = new Cliente("Atena", "PESSOA_JURIDICA");
+
+        System.out.println(cliente1);
+        System.out.println(cliente2);
+        System.out.println(cliente3);
+        System.out.println(cliente4);
 
     }
 }
