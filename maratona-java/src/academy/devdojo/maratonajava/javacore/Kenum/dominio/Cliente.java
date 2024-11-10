a1edecda1767cd38c0df0f6102b3f7bf1f0d04b0
Caique <caiquegomesdev@gmail.com>
2024-11-09 23:45:40 -0300

    Aula80 - Orientação Objetos - Enumeração pt 01 - Introdução

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Kenum.dominio;
+
+public class Cliente {
+    private String nome;
+    private String tipo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
