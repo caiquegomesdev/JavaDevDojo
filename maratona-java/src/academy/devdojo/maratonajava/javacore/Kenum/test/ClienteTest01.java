6b96634005c5f5226e74ca0ecd727c84c4550189
Caique <caiquegomesdev@gmail.com>
2024-11-10 21:26:19 -0300

    Aula83 - Orientação Objetos - Enumeração pt 04 - Busca por atributos

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -19,5 +19,11 @@ public class ClienteTest01 {
         System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
         System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
 
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente.getNomeRelatorio());
+
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Fisica222");
+        System.out.println(tipoCliente2);
+
     }
 }
