ba4028a5a6bc7817610e22225c037647f400160c
Caique <caiquegomesdev@gmail.com>
2024-11-11 23:53:48 -0300

    Aula83 - Orientação Objetos - Enumeração pt 04 - Busca por atributos

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -12,9 +12,14 @@ public class ClienteTest01 {
         Cliente cliente4 = new Cliente("Atena", TipoCliente.PESSOA_JURIDICA, TipoPagamento.DEBITO);
 
         System.out.println(cliente1);
+        System.out.println("-------------------------------------------------");
         System.out.println(cliente2);
+        System.out.println("-------------------------------------------------");
         System.out.println(cliente3);
+        System.out.println("-------------------------------------------------");
         System.out.println(cliente4);
+        System.out.println("-------------------------------------------------");
+
 
         System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
         System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
