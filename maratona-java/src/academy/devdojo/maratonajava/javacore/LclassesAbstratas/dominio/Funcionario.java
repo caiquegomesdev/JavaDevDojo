b7ae5f6710bfe6188d795aa9aa807905d3364039
Caique <caiquegomesdev@gmail.com>
2024-11-13 22:55:49 -0300

    Aula86 - Orientação Objetos - Classes abstratas pt 03 - Métodos abstratos regras

--- maratona-java/src/academy/devdojo/maratonajava/javacore/LclassesAbstratas/dominio/Funcionario.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/LclassesAbstratas/dominio/Funcionario.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.javacore.LclassesAbstratas.dominio;
 
-public abstract class Funcionario {
+public abstract class Funcionario extends Pessoa {
     protected String nome;
     protected double salario;
 
@@ -11,5 +11,18 @@ public abstract class Funcionario {
     }
 
     public abstract void calculaBonus();
+
+    @Override
+    public void imprime() {
+        System.out.println("Imprimindo...");
+    }
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+
 
 }
