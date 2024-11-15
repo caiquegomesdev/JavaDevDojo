91323da99f2cbc848b9d41597e3697d5bd9d6722
Caique <caiquegomesdev@gmail.com>
2024-11-14 23:38:49 -0300

    88 - Orientação Objetos - Interfaces pt 02 - Implementando múltiplas interfaces

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
@@ -1,9 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
 
-public class DatabaseLoader implements DataLoader{
+public class DatabaseLoader implements DataLoader, DataRemove{
     @Override
     public void load() {
         System.out.println("CARREGANDO DADOS DO BANCO DE DADOS ...");
     }
 
+    @Override
+    public void remove() {
+        System.out.println("REMOVENDO DO BANCO DE DADOS");
+    }
 }
