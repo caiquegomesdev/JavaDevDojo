e3d8d6abcfc9bea8842356b763dadf960e332eb9
Caique <caiquegomesdev@gmail.com>
2024-11-14 23:39:51 -0300

    88 - Orientação Objetos - Interfaces pt 02 - Implementando múltiplas interfaces

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/FileLoader.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/FileLoader.java
@@ -1,8 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
 
-public class FileLoader implements DataLoader {
+public class FileLoader implements DataLoader,DataRemove {
     @Override
     public void load() {
         System.out.println("CARREGANDO DADOS DE UM ARQUIVO...");
+    }
+
+    @Override
+    public void remove() {
+        System.out.println("DELETANDO DADOS DE UM ARQUIVO");
     }
 }
