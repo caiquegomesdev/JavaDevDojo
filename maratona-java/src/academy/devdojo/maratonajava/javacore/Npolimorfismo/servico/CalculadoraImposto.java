e4f7fad8638162f7526c128c5fb2a470e75aa253
Caique <caiquegomesdev@gmail.com>
2024-11-18 22:05:42 -0300

    Aula92 - Orientação Objetos - Polimorfismo pt 03 - Parâmetros polimórficos

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -10,7 +10,7 @@ public class CalculadoraImposto {
         System.out.println("Relatorio de Imposto");
         double imposto = produto.calcularImposto();
         System.out.println("Produto: " + produto.getNome());
-        System.out.println("Produto: " + produto.getValor());
+        System.out.println("Preço: " + produto.getValor());
         System.out.println("Imposto a ser pago: " + imposto);
     }
 }
