c33d585723f16fcae493983aaca6e0bbe3f8fb6e
Caique <caiquegomesdev@gmail.com>
2024-11-20 22:03:29 -0300

    Aula96 - Exceções pt 02 - RuntimeException

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -3,6 +3,7 @@ package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
 public class RuntimeExceptionTest01 {
     public static void main(String[] args) {
         // Checked e Unchecked
-        
+        Object object = null;
+        System.out.println(object.toString());
     }
 }
