7be1f73fc48230f9583b49e07eca47d62a03c314
Caique <caiquegomesdev@gmail.com>
2024-11-21 23:25:16 -0300

    Aula98 - Exceções pt 04 - Lançando exceção unchecked

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest02 {
+    public static void main(String[] args) {
+        System.out.println( divisao(1,0));
+    }
+
+    private static int divisao(int a, int b){
+        
+        try{
+            return a/b;
+        } catch(RuntimeException e){
+            e.printStackTrace();
+        }
+        return 0;
+
+    }
+}
