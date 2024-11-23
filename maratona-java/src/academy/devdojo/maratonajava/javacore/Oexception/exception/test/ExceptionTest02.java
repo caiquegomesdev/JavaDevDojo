fb69478348fbe684544ae9bf4c493967638f18f6
Caique <caiquegomesdev@gmail.com>
2024-11-22 22:07:35 -0300

    Aula99 - Exceções pt 05 - Lançando exceção checked

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest02.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest02.java
@@ -8,14 +8,14 @@ public class ExceptionTest02 {
         criarNovoArquivo();
     }
 
-    private static void criarNovoArquivo() throws IOException{
+    public static void criarNovoArquivo() throws IOException{
         File file = new File("arquivo\\teste.txt");
         try{
             boolean isCriado = file.createNewFile();
             System.out.println("Arquivo criado " +isCriado);
         }catch (IOException e){
             e.printStackTrace();
-
+            throw e;
         }
     }
 }
