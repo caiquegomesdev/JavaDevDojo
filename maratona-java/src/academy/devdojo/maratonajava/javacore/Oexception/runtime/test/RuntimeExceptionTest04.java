3d010f19b570180f41c7c340fa3e851a5c2abc2a
Caique <caiquegomesdev@gmail.com>
2024-11-23 23:55:07 -0300

    Aula101 - Exceções pt 07 - Capturando múltiplas exceções

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest04 {
+    public static void main(String[] args) {
+        try{
+
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("DENTRO DO ArrayIndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("DENTRO DO IndexOutOfBoundsException");
+        } catch (IllegalArgumentException e){
+            System.out.println("DENTRO DO IllegalArgumentException");
+        }catch (ArithmeticException e){
+            System.out.println("DENTRO DO ArithmeticException");
+        }
+
+    }
+
+}
