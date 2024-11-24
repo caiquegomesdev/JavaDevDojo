174ff6e5723198e61ac35e1b9d9fec35f5e064ee
Caique <caiquegomesdev@gmail.com>
2024-11-23 23:47:53 -0300

    Aula100 - Exceções pt 06 - Bloco Finally

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest03.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest03.java
@@ -2,7 +2,9 @@ package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
 
 public class RuntimeExceptionTest03 {
     public static void main(String[] args) {
-
+        abreConexao();
+        System.out.println("----------------------------");
+        abreConexao2();
     }
 
     private static String abreConexao(){
@@ -16,5 +18,17 @@ public class RuntimeExceptionTest03 {
             System.out.println("Fechando recuso liberado pelo SO");
         }
         return null;
+    }
+
+    private static String abreConexao2(){
+        try{
+            System.out.println("Abrindo Arquivo");
+            throw new RuntimeException();
+            // System.out.println("Escrevendo dados no Arquivo");
+
+        }finally {
+            System.out.println("Fechando recuso liberado pelo SO");
+        }
+
     }
 }
