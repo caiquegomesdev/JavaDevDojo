3963acbef8649fabc5d44c08aa3cad43d1c7ee25
Caique <caiquegomesdev@gmail.com>
2024-11-25 22:20:42 -0300

    Aula102 - Exceções pt 08 - Multi catch em linha

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -1,34 +1,27 @@
 package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         try{
             throw new RuntimeException();
-        }catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("DENTRO DO ArrayIndexOutOfBoundsException");
-        } catch (IndexOutOfBoundsException e){
-            System.out.println("DENTRO DO IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e){
-            System.out.println("DENTRO DO IllegalArgumentException");
-        }catch (ArithmeticException e){
-            System.out.println("DENTRO DO ArithmeticException");
+        }catch(ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e){
+            System.out.println("DENTRO DO ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException");
         } catch (RuntimeException e){
             System.out.println("DENTRO DO RuntimeException");
         }
 
         try {
             talvezLanceException();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } catch (FileNotFoundException e) {
+        } catch (Exception  e) {
             e.printStackTrace();
         }
     }
 
-    private static void talvezLanceException() throws SQLException, FileNotFoundException {
+    private static void talvezLanceException() throws SQLException, IOException{
 
     }
 
