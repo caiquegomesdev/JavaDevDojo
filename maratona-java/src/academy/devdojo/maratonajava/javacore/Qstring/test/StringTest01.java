231bbf4eab89ffe37471a41072afaa2c0d2c452a
Caique <caiquegomesdev@gmail.com>
2024-11-28 20:58:19 -0300

    Aula108 - Classes Utilitárias - Strings pt 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -5,12 +5,11 @@ public class StringTest01 {
     public static void main(String[] args) {
         String nome = "Gomes"; // String constant pool
         String nome2 = "Gomes";
-
         nome = nome.concat( " Caique");
-
         System.out.println(nome);
         System.out.println(nome == nome2);
-
+        String nome3 = new String("Gomes");
+        System.out.println(nome2 == nome3);
     }
 
 }
