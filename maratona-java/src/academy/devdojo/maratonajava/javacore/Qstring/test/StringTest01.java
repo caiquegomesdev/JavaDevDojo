39187340d798b9e9bbc48b9a37eb2bd90fd340ea
Caique <caiquegomesdev@gmail.com>
2024-11-28 20:54:12 -0300

    Aula108 - Classes Utilitárias - Strings pt 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest01 {
+
+    public static void main(String[] args) {
+        String nome = "Gomes"; // String constant pool
+        String nome2 = "Gomes";
+
+        System.out.println(nome == nome2);
+
+    }
+
+}
