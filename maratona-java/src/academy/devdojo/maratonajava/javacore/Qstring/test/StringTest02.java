94b9a34f7132ae5939f23aceab160b8667740930
Caique <caiquegomesdev@gmail.com>
2024-11-29 23:16:56 -0300

    Aula109 - Classes Utilitárias - Strings pt 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest02.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest02.java
@@ -3,10 +3,14 @@ package academy.devdojo.maratonajava.javacore.Qstring.test;
 public class StringTest02 {
     public static void main(String[] args) {
         String nome = "Luffy";
+        String numeros = "0123456";
         System.out.println(nome.charAt(0));
         System.out.println(nome.length());
         System.out.println(nome.replace("f", "c"));
         System.out.println(nome.toLowerCase());
         System.out.println(nome.toUpperCase());
+        System.out.println(numeros.length());
+        System.out.println(numeros.substring(0,numeros.length()));
+        System.out.println(numeros.substring(numeros.length()));
     }
 }
