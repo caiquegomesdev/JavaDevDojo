b0d07430c3b884edef1396e5ffed5998bdcec8f9
Caique <caiquegomesdev@gmail.com>
2024-11-29 23:07:17 -0300

    Aula109 - Classes Utilitárias - Strings pt 02

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest02.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest02 {
+    public static void main(String[] args) {
+        String nome = "Luffy";
+        System.out.println(nome.charAt(0));
+        System.out.println(nome.length());
+    }
+}
