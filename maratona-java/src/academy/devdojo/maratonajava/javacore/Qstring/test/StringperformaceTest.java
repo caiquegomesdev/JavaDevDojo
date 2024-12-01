d4527fa69bcb96b06aed1185ff2a43d48ec4757d
Caique <caiquegomesdev@gmail.com>
2024-12-01 12:21:25 -0300

    Aula110 - Classes Utilitárias - Strings pt 03 - Desempenho

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Qstring/test/StringperformaceTest.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringperformaceTest {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concatString(100_000);
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String " +(inicio - fim) + "ms");
+    }
+    
+    private static void concatString(int tamanho){
+        String texto = " ";
+        for (int i = 0; i < tamanho; i++) {
+            texto +=i;
+            
+        }
+    }
+
+    
+}
