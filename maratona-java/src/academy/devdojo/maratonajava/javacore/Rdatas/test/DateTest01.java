c190120ef9e3270948194bf711558e6b55599c1c
Caique <caiquegomesdev@gmail.com>
2024-12-02 22:24:33 -0300

    Aula112 - Classes Utilitárias - Date

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/DateTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/DateTest01.java
@@ -4,7 +4,8 @@ import java.util.Date;
 
 public class DateTest01 {
     public static void main(String[] args) {
-        Date date = new Date(1_000_000_000_000L);
-        System.out.println(date.getTime());
+        Date date = new Date();
+        date.setTime(date.getTime() + 3_600_000);
+        System.out.println(date);
     }
 }
