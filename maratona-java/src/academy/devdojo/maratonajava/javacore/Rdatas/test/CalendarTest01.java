6b67d7c3eb277451e0c8fa17cce9cde1f23b8c4f
Caique <caiquegomesdev@gmail.com>
2024-12-03 22:43:58 -0300

    Aula113 - Classes Utilitárias - Calendar

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/CalendarTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarTest01 {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        Date date = c.getTime();
+        System.out.println(date);
+    }
+}
