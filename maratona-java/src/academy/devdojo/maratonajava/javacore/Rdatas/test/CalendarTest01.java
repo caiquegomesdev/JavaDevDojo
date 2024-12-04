65bb35a73a3cbb2c3ae9e6991c1588adbb1ee6c4
Caique <caiquegomesdev@gmail.com>
2024-12-03 22:48:07 -0300

    Aula113 - Classes Utilitárias - Calendar

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/CalendarTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/CalendarTest01.java
@@ -9,6 +9,12 @@ public class CalendarTest01 {
         if(c.getFirstDayOfWeek() == Calendar.SUNDAY){
             System.out.println("Domingo é o primeiro dia da semana");
         }
+        
+        System.out.println(c.get(Calendar.DAY_OF_WEEK));
+        System.out.println(c.get(Calendar.DAY_OF_MONTH));
+        System.out.println(c.get(Calendar.DAY_OF_YEAR));
+        System.out.println(c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+
         Date date = c.getTime();
         System.out.println(date);
     }
