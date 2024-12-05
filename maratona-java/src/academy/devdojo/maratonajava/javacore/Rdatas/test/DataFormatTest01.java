774014674e1d04e0ca0b8354399d3687c3c603ff
Caique <caiquegomesdev@gmail.com>
2024-12-04 23:43:31 -0300

    Aula114 - Classes Utilitárias - DateFormat

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/DataFormatTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/DataFormatTest01.java
@@ -6,14 +6,17 @@ import java.util.Calendar;
 public class DataFormatTest01 {
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
-        DateFormat[] df = new DateFormat[6];
-        df [0] = DateFormat.getInstance() ;
-        df [1] = DateFormat.getDateInstance();
-        df [2] = DateFormat.getDateTimeInstance();
-        df [3] = DateFormat.getDateInstance(DateFormat.SHORT);
-        df [4] = DateFormat.getDateInstance(DateFormat.MEDIUM);
-        df [5] = DateFormat.getDateInstance(DateFormat.LONG);
-        df [6] = DateFormat.getDateInstance(DateFormat.FULL);
+        DateFormat[] df = new DateFormat[7];
+        df[0] = DateFormat.getInstance() ;
+        df[1] = DateFormat.getDateInstance();
+        df[2] = DateFormat.getDateTimeInstance();
+        df[3] = DateFormat.getDateInstance(DateFormat.SHORT);
+        df[4] = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        df[5] = DateFormat.getDateInstance(DateFormat.LONG);
+        df[6] = DateFormat.getDateInstance(DateFormat.FULL);
 
+        for (DateFormat dateFormat: df){
+            System.out.println(dateFormat.format(calendar.getTime()));
+        }
     }
 }
