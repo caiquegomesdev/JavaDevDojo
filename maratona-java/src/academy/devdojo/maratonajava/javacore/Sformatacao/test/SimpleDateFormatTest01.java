daa08380aec8cd67497148340bf17667ba54e316
Caique <caiquegomesdev@gmail.com>
2024-12-08 12:18:29 -0300

    Aula118 - Classes Utilitárias - SimpleDateFormat

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Sformatacao/test/SimpleDateFormatTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Sformatacao/test/SimpleDateFormatTest01.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class SimpleDateFormatTest01 {
     public static void main(String[] args) {
-        String pattern = "yyyy.MM.dd G 'at' HH:mm:ss z";
+        String pattern = "'Amsterdam' dd 'de' MMMM 'de' yyyy";
         SimpleDateFormat sdf = new SimpleDateFormat(pattern);
 
         System.out.println(sdf.format(new Date()));
