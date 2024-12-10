640135d0df92eaaebaac439b4758a8c2dacb8dad
Caique <caiquegomesdev@gmail.com>
2024-12-09 21:29:54 -0300

    Aula119 - Classes Utilitárias - LocalDate

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalDateTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Calendar;
+import java.util.Date;
+
+public class LocalDateTest01 {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        System.out.println(Calendar.getInstance());
+        System.out.println(Month.JANUARY.getValue());
+        LocalDate date = LocalDate.of(2025, Month.JANUARY, 01);
+        System.out.println(date.getYear());
+        System.out.println(date.getMonth());
+    }
+}
