8313dbf04a9b021f785db28b4fae69be57f12d61
Caique <caiquegomesdev@gmail.com>
2024-12-11 22:33:36 -0300

    Aula122 - Classes Utilitárias - Instant

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/InstantTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class InstantTest01 {
+    public static void main(String[] args) {
+        Instant now = Instant.now();
+        System.out.println(now);
+        System.out.println(LocalDateTime.now());
+    }
+}
