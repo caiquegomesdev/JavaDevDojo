73ee5ea3b12de3c69c56da76778260f2902e4ac0
Caique <caiquegomesdev@gmail.com>
2024-12-11 22:36:15 -0300

    Aula122 - Classes Utilitárias - Instant

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/InstantTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/InstantTest01.java
@@ -11,6 +11,9 @@ public class InstantTest01 {
         System.out.println(LocalDateTime.now());
         System.out.println(now.getEpochSecond());
         System.out.println(now.getNano());
-        
+
+        System.out.println(Instant.ofEpochSecond(3,0));
+        System.out.println(Instant.ofEpochSecond(3,1_000_000_000));
+        System.out.println(Instant.ofEpochSecond(3,-1_000_000_000));
     }
 }
