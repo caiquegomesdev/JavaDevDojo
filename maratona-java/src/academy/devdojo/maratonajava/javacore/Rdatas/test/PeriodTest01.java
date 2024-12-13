bbb5624d4425719a90a78b5e11004957271fb99b
Caique <caiquegomesdev@gmail.com>
2024-12-13 20:10:56 -0300

    Aula124 - Classes Utilitárias - Period

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
@@ -8,10 +8,10 @@ public class PeriodTest01 {
         LocalDate now = LocalDate.now();
         LocalDate nowAfterTwoYears = LocalDate.now().plusYears(2).plusDays(7);
         Period p1 = Period.between(now, nowAfterTwoYears);
-
+        Period p2 = Period.ofDays(10);
 
         System.out.println(p1);
-        
+        System.out.println(p2);
 
     }
 }
