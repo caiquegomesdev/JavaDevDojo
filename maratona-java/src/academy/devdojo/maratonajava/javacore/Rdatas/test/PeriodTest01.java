216f69dab12f8ea7763878ce3f9cfff7f7e1e0ab
Caique <caiquegomesdev@gmail.com>
2024-12-13 20:12:48 -0300

    Aula124 - Classes Utilitárias - Period

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
@@ -10,10 +10,14 @@ public class PeriodTest01 {
         Period p1 = Period.between(now, nowAfterTwoYears);
         Period p2 = Period.ofDays(10);
         Period p3 = Period.ofWeeks(58);
+        Period p4 = Period.ofMonths(3);
+        Period p5 = Period.ofYears(3);
 
         System.out.println(p1);
         System.out.println(p2);
         System.out.println(p3);
+        System.out.println(p4);
+        System.out.println(p5);
 
     }
 }
