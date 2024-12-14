89a95c33f821d63715beba42d8ee6fc16624b08f
Caique <caiquegomesdev@gmail.com>
2024-12-14 17:57:52 -0300

    Aula126 - Classes Utilitárias - TemporalAdjusters

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/TemporalAdjustersTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/TemporalAdjustersTest01.java
@@ -1,11 +1,12 @@
 package academy.devdojo.maratonajava.javacore.Rdatas.test;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoField;
 
 public class TemporalAdjustersTest01 {
     public static void main(String[] args) {
         LocalDate now = LocalDate.now();
-        now = now.plusDays(2);
+        now = now.with(ChronoField.DAY_OF_MONTH, 18);
         System.out.println(now);
         System.out.println(now.getDayOfWeek());
     }
