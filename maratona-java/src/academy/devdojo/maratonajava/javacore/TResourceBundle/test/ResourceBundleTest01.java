2f268effff8f1a78a0c66a981d02e38d5c30b703
Caique <caiquegomesdev@gmail.com>
2024-12-18 21:29:12 -0300

    Aula130 - Classes Utilitárias - ResourceBundle

--- maratona-java/src/academy/devdojo/maratonajava/javacore/TResourceBundle/test/ResourceBundleTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/TResourceBundle/test/ResourceBundleTest01.java
@@ -6,13 +6,16 @@ import java.util.ResourceBundle;
 public class ResourceBundleTest01 {
     public static void main(String[] args) {
         System.out.println(Locale.getDefault());
-       ResourceBundle bundle = ResourceBundle.getBundle("messages", new Locale("en","US"));
-
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", new Locale("en","US"));
         System.out.println( bundle.getString("hello") );
         System.out.println( bundle.getString("good.morning") );
+
+        System.out.println(bundle.getString("hi"));
 
         bundle = ResourceBundle.getBundle("messages", new Locale("pt","BR"));
         System.out.println( bundle.getString("hello") );
         System.out.println( bundle.getString("good.morning") );
+
+        System.out.println(bundle.getString("hi"));
     }
 }
