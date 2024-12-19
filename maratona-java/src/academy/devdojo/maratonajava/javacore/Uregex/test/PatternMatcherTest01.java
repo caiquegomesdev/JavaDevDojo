da4575aafe9be216437472cf8c07eea669943f3b
Caique <caiquegomesdev@gmail.com>
2024-12-18 21:47:51 -0300

    Aula131 - Classes Utilitárias - Regex pt 01 - Pattern e Matcher

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest01.java
@@ -5,12 +5,13 @@ import java.util.regex.Pattern;
 
 public class PatternMatcherTest01 {
     public static void main(String[] args) {
-        String regex = "ab";
-        String texto = "abaaba";
+        String regex = "aba";
+        //String texto = "abaaba";
+        String texto2 = "abababa";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(texto);
+        Matcher matcher = pattern.matcher(texto2);
 
-        System.out.println("texto:  " + texto);
+        System.out.println("texto:  " + texto2);
         System.out.println("indice: 0123456789");
         System.out.println("regex "+regex);
         System.out.println("Posicoes encontradas");
