898d7808d143d29d65a972b49d06d6cac31cef18
Caique <caiquegomesdev@gmail.com>
2024-12-20 22:08:48 -0300

    Aula133 - Classes Utilitárias - Regex pt 03 - Pattern e Matcher - Range

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest02 {
+    public static void main(String[] args) {
+        // \d = Todos os digitos
+        // \D = Tudo que nao for digito
+        // \s = Espacos em branco \t \n \f \r
+        // \S = Todos os caracters excluindo os brancos
+        // \w = a-ZA-Z, digitos, _
+        // \W = Tudo o que nao for incluso no \w
+        String regex = "\\w";
+        //String texto = "abaaba";
+        String texto2 = "a23@\n%67t0";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto2);
+
+        System.out.println("texto:  " + texto2);
+        System.out.println("indice: 0123456789");
+        System.out.println("regex "+regex);
+        System.out.println("Posicoes encontradas");
+        while (matcher.find()){
+            System.out.println(matcher.start()+ " " +matcher.group()+"\n");
+        }
+
+    }
+}
