f4fd728fae456714f3db6311b42cd31e21586c0f
Caique <caiquegomesdev@gmail.com>
2024-12-24 17:40:58 -0300

    Aula135 - Classes Utilitárias - Regex pt 05 - Pattern e Matcher - Quantificadores pt 02

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest05.java
@@ -0,0 +1,39 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest05 {
+    public static void main(String[] args) {
+        // \d = Todos os digitos
+        // \D = Tudo que nao for digito
+        // \s = Espacos em branco \t \n \f \r
+        // \S = Todos os caracters excluindo os brancos
+        // \w = a-ZA-Z, digitos, _
+        // \W = Tudo o que nao for incluso no \w
+        // []
+        // ? = Zero ou uma
+        // * = Zero ou mais
+        // + = um ou mais
+        // {n,m} de n ate m
+        // ()
+        // | o(v|c)o ovo | oco
+        // $
+        // . 1.3 = 123, 133, 1@3, 1A3
+        String regex = "0[xX]([0-9a-fA-F])+(\\s|$)";
+        String texto = "Caiquegomesdev@gmail.com, 123jotaro@gmail.com, #@!zoro@gmail.com, teste@gmail.com, sukuna@mail ";
+        Pattern pattern = Pattern.compile(regex);
+        //Matcher matcher = pattern.matcher(texto2);
+        Matcher matcher = pattern.matcher(texto);
+
+        System.out.println("texto:  " + texto);
+        System.out.println("indice: 0123456789");
+        System.out.println("regex "+regex);
+        System.out.println("Posicoes encontradas");
+        while (matcher.find()){
+            System.out.print(matcher.start()+ " " +matcher.group()+"\n");
+        }
+
+
+    }
+}
