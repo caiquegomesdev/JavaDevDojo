53aa04a7a31ef91ab3d329e0d2bc8c61f17ae11c
Caique <caiquegomesdev@gmail.com>
2024-12-26 23:19:44 -0300

    Aula138 - Classes Utilitárias - IO pt 01 - File

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Vio/test/FileTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Vio/test/FileTest01.java
@@ -4,12 +4,16 @@ import java.io.File;
 import java.io.IOException;
 
 public class FileTest01 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         File file = new File("file.txt");
         try {
-            boolean isDeleted = file.delete();
+
             boolean isCreated = file.createNewFile();
-            System.out.println(isCreated);
+            System.out.println("Created " +isCreated);
+            boolean exists = file.exists();
+            if (exists) {
+                System.out.println("Deleted " + file.delete());
+            }
         } catch (IOException exception){
             exception.printStackTrace();
         }
