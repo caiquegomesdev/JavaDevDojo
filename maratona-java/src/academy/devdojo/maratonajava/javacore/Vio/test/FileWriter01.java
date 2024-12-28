a39871d3fd3150ea5bfe6cf0329bcf5402b78614
Caique <caiquegomesdev@gmail.com>
2024-12-27 21:14:10 -0300

    Aula139 - Classes Utilitárias - IO pt 02 - FileWriter

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Vio/test/FileWriter01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+//File
+//FileWriter
+//FileReader
+//BufferedWriter
+//BufferedReader
+
+
+public class FileWriter01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try {
+            FileWriter fw = new FileWriter(file);
+            fw.write("Caique Gomes é o melhor dev do Brasil");
+            
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
