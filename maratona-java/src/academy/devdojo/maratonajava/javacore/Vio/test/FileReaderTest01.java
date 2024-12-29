8a45ce9b944d6026870252f8e6a0e73cc9c6fd8b
Caique <caiquegomesdev@gmail.com>
2024-12-28 21:22:53 -0300

    Aula140 - Classes Utilitárias - IO pt 03 - FileReader

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Vio/test/FileReaderTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Vio/test/FileReaderTest01.java
@@ -6,7 +6,7 @@ public class FileReaderTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileReader fr = new FileReader(file)) {
-           char[] in = new char[1];
+           char[] in = new char[30];
             fr.read(in);
             for (char c : in){
                 System.out.println(c);
