62eff2abe84d159262025ac12590ccd56a4ec306
Caique <caiquegomesdev@gmail.com>
2024-12-30 22:15:13 -0300

    Aula141 - Classes Utilitárias - IO pt 04 - BufferedWriter

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedWriterTest01 {
+    public static void main(String[] args) {
+        File file  = new File("file.txt");
+        try (FileWriter fw = new FileWriter(file, true);
+             BufferedWriter br = new BufferedWriter(fw)) {
+            fw.write("Caique Gomes é o melhor dev do Brasil\n continuando...");
+            fw.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
