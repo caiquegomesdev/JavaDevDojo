dfa3916cd5991af7ce9385b43a619bb0f7791f03
Caique <caiquegomesdev@gmail.com>
2024-12-31 18:13:13 -0300

    Aula145 - Classes Utilitárias - NIO pt 02 - Path, Paths, Files pt 02

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Wnio/test/PathTest02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest02 {
+    public static void main(String[] args) throws IOException {
+        Path pastaPath = Paths.get("pasta");
+        if (Files.notExists(pastaPath)) {
+            Path pastaDirectory = Files.createDirectory(pastaPath);
+        }
+        
+    }
+}
