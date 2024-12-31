e967bc6397848c90fa02096967cb39f0fe2f6658
Caique <caiquegomesdev@gmail.com>
2024-12-31 13:34:51 -0300

    Aula143 - Classes Utilitárias - IO pt 06 - File para diretórios

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Vio/test/FileTest02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileTest02 {
+    public static void main(String[] args) throws IOException {
+        File fileDiretorio = new File("pasta");
+        boolean isDiretorioCreated = fileDiretorio.mkdir();
+        System.out.println(isDiretorioCreated);
+        File fileArquivoDiretorio = new File("C:\\Users\\Administrator\\Documents\\CursoDevDojo(MaratonaDevJAVA)\\maratona-java\\pasta\\arquivo.txt");
+        boolean isFileCreated = fileArquivoDiretorio.createNewFile();
+        System.out.println(isFileCreated);
+    }
+}
