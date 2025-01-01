9a4bedf2278f99a94a1e2a61a5488565612b1f04
Caique <caiquegomesdev@gmail.com>
2025-01-01 10:54:59 -0300

    Aula146 - Classes Utilitárias - NIO pt 03 - Normalização

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Wnio/test/NormalizeTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Wnio/test/NormalizeTest01.java
@@ -9,5 +9,6 @@ public class NormalizeTest01 {
         String arquivoTxt = "../../arquivo.txt";
         Path path1 = Paths.get(diretorioProjeto,arquivoTxt);
         System.out.println(path1);
+        System.out.println(path1.normalize());
     }
 }
