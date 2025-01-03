b27376d151aa02735a2ef64f2055a8f050965a76
Caique Gomes <caiquegomesdev@gmail.com>
2025-01-03 20:33:01 -0300

    Aula148 - Classes Utilitárias - NIO pt 05 - Relativize

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Wnio/test/RelativizeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class RelativizeTest01 {
+    public static void main(String[] args) {
+        Path dir = Paths.get("/home/ad");
+    }
+}
