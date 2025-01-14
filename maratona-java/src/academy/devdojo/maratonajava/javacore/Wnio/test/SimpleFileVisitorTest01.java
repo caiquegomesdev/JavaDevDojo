c184dabf219d918910186d405d1015767544e365
Gomes <caiquegomesdev@gmail.com>
2025-01-13 21:55:19 -0300

    Aula154 - Classes Utilitárias - NIO pt 11 - SimpleFileVisitor pt 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Wnio/test/SimpleFileVisitorTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Wnio/test/SimpleFileVisitorTest01.java
@@ -8,14 +8,14 @@ import java.nio.file.attribute.BasicFileAttributes;
 class ListAllFiles extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-        return super.visitFile(file, attrs);
+        System.out.println(file.getFileName());
+        return FileVisitResult.CONTINUE;
     }
 }
 
 public class SimpleFileVisitorTest01 {
-    public static void main(String[] args) {
-        Paths.get(".");
-        Files.walkFileTree(root, );
-
+    public static void main(String[] args) throws IOException {
+        Path root = Paths.get(".");
+        Files.walkFileTree(root, new ListAllFiles());
     }
 }
