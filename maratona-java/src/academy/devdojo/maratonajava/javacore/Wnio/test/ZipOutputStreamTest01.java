30b9b0d44c8e9a346f39fddf0295414d255595b2
Gomes <caiquegomesdev@gmail.com>
2025-01-18 00:14:28 -0300

    Aula158 - Classes Utilitárias - NIO pt 15 - ZipOutputStream

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Wnio/test/ZipOutputStreamTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Wnio/test/ZipOutputStreamTest01.java
@@ -1,10 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Wnio.test;
 
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -12,17 +9,29 @@ public class ZipOutputStreamTest01 {
     public static void main(String[] args) {
         Path arquivoZip = Paths.get("pasta/arquivo.zip");
         Path arquivosParaZipar = Paths.get("pasta/subpasta1/subsubpasta1");
+
+        zip(arquivoZip, arquivosParaZipar);  // Chamada do método zip()
     }
-    private  static void zip(Path arquivoZip, Path arquivosParaZipar){
-        try(ZipOutputStream zipStream = new ZipOutputStream(Files.newOutputStream(arquivoZip));
-            DirectoryStream<Path> directoryStream = Files.newDirectoryStream(arquivosParaZipar)){
-            for(Path file: directoryStream ){
-                ZipEntry zipEntry = new ZipEntry(file.getFileName().toString());
-                zipStream.putNextEntry(zipEntry);
-                zipStream.closeEntry();
+
+    private static void zip(Path arquivoZip, Path arquivosParaZipar) {
+        try {
+            // Criando os diretórios necessários para o arquivo ZIP
+            Files.createDirectories(arquivoZip.getParent());
+
+            try (ZipOutputStream zipStream = new ZipOutputStream(Files.newOutputStream(arquivoZip));
+                 DirectoryStream<Path> directoryStream = Files.newDirectoryStream(arquivosParaZipar)) {
+
+                for (Path file : directoryStream) {
+                    if (Files.isRegularFile(file)) {  // Evita tentar zipar diretórios
+                        ZipEntry zipEntry = new ZipEntry(file.getFileName().toString());
+                        zipStream.putNextEntry(zipEntry);
+                        Files.copy(file, zipStream);
+                        zipStream.closeEntry();
+                    }
+                }
+                System.out.println("Arquivo ZIP criado com sucesso em: " + arquivoZip.toAbsolutePath());
             }
-            System.out.println("Arquivo criado com sucesso!");
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
