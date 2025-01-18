536efa04b86bcf53ff70b8fe743a8953efc0407b
Gomes <caiquegomesdev@gmail.com>
2025-01-17 23:48:32 -0300

    Aula158 - Classes Utilitárias - NIO pt 15 - ZipOutputStream

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Wnio/test/ZipOutputStreamTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ZipOutputStreamTest01 {
+    public static void main(String[] args) {
+        Path arquivoZip = Paths.get("pasta/arquivo.zip");
+        Paths.get("pasta/subpasta1/subsubpasta1");
+    }
+    private  static void zip(){
+
+    }
+}
