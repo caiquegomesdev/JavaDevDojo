f6d632c92669ccb5da2b26d2ed88461b7e197b19
Gomes <caiquegomesdev@gmail.com>
2025-01-19 13:19:03 -0300

    Aula159 - Classes Utilitárias - Serialization pt 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Xserializacao/test/SerializacaoTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Xserializacao/test/SerializacaoTest01.java
@@ -1,7 +1,24 @@
 package academy.devdojo.maratonajava.javacore.Xserializacao.test;
 
+import academy.devdojo.maratonajava.javacore.Xserializacao.dominio.Aluno;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class SerializacaoTest01 {
     public static void main(String[] args) {
-        
+        Aluno aluno = new Aluno(1L, "Caique Gomes", "gomes123.2025");
+        serializar(aluno);
+    }
+    private  static void serializar(Aluno aluno){
+        Path path = Paths.get("aluno.ser");
+        try(ObjectOutputStream oos = new  ObjectOutputStream(Files.newOutputStream(path))){
+            oos.writeObject(aluno);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
