1488d639c57e2397ea9cf26c9f7ae278e70ddd1a
Gomes <caiquegomesdev@gmail.com>
2025-01-23 23:07:37 -0300

    Aula161 - Coleções pt 01 - equals pt 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest01.java
@@ -5,7 +5,7 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Smartphone;
 public class EqualsTest01 {
     public static void main(String[] args) {
         Smartphone s1 = new Smartphone("1A2EDF3","SAMSUNG" );
-        Smartphone s2 = s1;
+        Smartphone s2 = new Smartphone("1A2EDF3","SAMSUNG" );
         System.out.println(s1.equals(s2));
     }
 }
