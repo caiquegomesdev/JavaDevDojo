83f4a9dd0a199d8bd2c638e9559e4458a8064589
Gomes <caiquegomesdev@gmail.com>
2025-01-23 23:02:03 -0300

    Aula161 - Coleções pt 01 - equals pt 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+public class EqualsTest01 {
+    public static void main(String[] args) {
+        String nome1 = "Caique Gomes";
+        String nome2 = new String("Caique Gomes") ;
+        System.out.println(nome1 == nome2);
+    }
+}
