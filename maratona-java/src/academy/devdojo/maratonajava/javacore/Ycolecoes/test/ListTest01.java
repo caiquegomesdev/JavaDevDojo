27bafbab3d81c6a6f3f384ca25ca0633b5b62917
Gomes <caiquegomesdev@gmail.com>
2025-01-27 21:54:45 -0300

    Aula166 - Coleções pt 06 - List pt 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -5,13 +5,19 @@ import java.util.List;
 
 public class ListTest01 {
     public static void main(String[] args) {
-        List<String> nomes = new ArrayList<>();
-        nomes.add("Gomes");
+        List<String> nomes = new ArrayList<>(16);
+        nomes.add("Caique Gomes");
         nomes.add("Desenvolvedor Java");
 
 
         for(String nome: nomes){
             System.out.println(nome);
+        }
+        nomes.add("Fullstack");
+        System.out.println("-----------------");
+
+        for (int i = 0; i < nomes.size() ; i++) {
+            System.out.println(nomes.get(i));
         }
     }
 }
