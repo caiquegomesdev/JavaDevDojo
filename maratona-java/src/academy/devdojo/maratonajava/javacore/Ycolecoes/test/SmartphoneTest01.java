4e19e077d1652bb4b5cf8e403d64a0f3b14ea64e
Caique Gomes <caiquegomesdev@gmail.com>
2025-01-30 23:33:10 -0300

    Aula168 - Coleções pt 08 - List pt 03

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SmartphoneTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SmartphoneTest01.java
@@ -10,14 +10,20 @@ public class SmartphoneTest01 {
         Smartphone s1 = new Smartphone("1ABC1", "IPHONE");
         Smartphone s2 = new Smartphone("22222", "PIXEL");
         Smartphone s3 = new Smartphone("33333", "SAMSUNG");
-        Smartphone s4 = new Smartphone("44444", "XIAOMI");
+
         List<Smartphone> smartphones = new ArrayList<>(6);
         smartphones.add(s1);
         smartphones.add(s2);
         smartphones.add(s3);
-        smartphones.add(s4);
+
         for(Smartphone smartphone: smartphones){
             System.out.println(smartphone);
         }
+        Smartphone s4 = new Smartphone("44444", "XIAOMI");
+        smartphones.add(s4);
+        
+        System.out.println(smartphones.contains(s4));
+        int indexSmartphone4 = smartphones.indexOf(s4);
+        System.out.println(indexSmartphone4);
     }
 }
