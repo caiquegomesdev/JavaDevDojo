9022832560ec38fd4208e80ea14e6d9710fd7b74
Caique Gomes <caiquegomesdev@gmail.com>
2025-02-01 23:12:11 -0300

    Aula169 - Coleções pt 09 - Sorting lists pt 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListSortTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListSortTest01.java
@@ -18,11 +18,22 @@ public class ListSortTest01 {
         mangas.add("Dandadan");
         mangas.add("Mob psycho 100");
         mangas.add("One piece");
-        
+
         Collections.sort(mangas);
+
+        List<Double> dinheiros = new ArrayList<>();
+        dinheiros.add(100.21);
+        dinheiros.add(200.10);
+        dinheiros.add(21.21);
+        dinheiros.add(98.10);
 
         for(String manga : mangas){
             System.out.println(manga);
         }
+        System.out.println(dinheiros);
+        Collections.sort(dinheiros);
+        System.out.println(dinheiros);
+
+
     }
 }
