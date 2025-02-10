55188a6d7c9189570f1824c4cc96371965a1762a
Caique Gomes <caiquegomesdev@gmail.com>
2025-02-09 22:01:56 -0300

    Aula173 - Coleções pt 13 - Conversão de Lista para Arrays e vice versa

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListaArrayConversaoTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListaArrayConversaoTest01 {
+    public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        Integer[] integersArray = numeros.toArray(new Integer[0]);
+        System.out.println(Arrays.toString(integersArray));
+    }
+}
