a498eb1861ed3144bcfd0ab2c73b6c88ef585ae4
Caique Gomes <caiquegomesdev@gmail.com>
2025-02-15 00:06:25 -0300

    Aula176 - Coleções pt 16 - NavigableSet, TreeSet pt 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSeTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSeTest01.java
@@ -1,7 +1,17 @@
 package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Smartphone;
+
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
 public class NavigableSeTest01 {
     public static void main(String[] args) {
-        Navi
+        NavigableSet<Smartphone> set = new TreeSet<>();
+        Smartphone smartphone = new Smartphone("123", "Nokia");
+        set.add(smartphone);
+        NavigableSet<Manga> mangas = new TreeSet<>();
+        
     }
 }
