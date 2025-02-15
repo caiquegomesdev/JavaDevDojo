c954aeb97b7c795ba5814c428a12c8d5ba5839a0
Caique Gomes <caiquegomesdev@gmail.com>
2025-02-15 00:07:33 -0300

    Aula176 - Coleções pt 16 - NavigableSet, TreeSet pt 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSeTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSeTest01.java
@@ -12,6 +12,19 @@ public class NavigableSeTest01 {
         Smartphone smartphone = new Smartphone("123", "Nokia");
         set.add(smartphone);
         NavigableSet<Manga> mangas = new TreeSet<>();
-        
+        mangas.add(new Manga(5L,"Attack on titan",19.9));
+        mangas.add(new Manga(3L,"Villand Saga",9.5));
+        mangas.add(new Manga(1L,"Berserk",29.9));
+        mangas.add(new Manga(4L,"Ousama Ranking",18.9));
+        mangas.add(new Manga(6L,"Hunte X Hunter",20.0));
+        mangas.add(new Manga(8L,"Cowboy bebop",12.5));
+        mangas.add(new Manga(7L,"Akira",20.9));
+        mangas.add(new Manga(10L,"One punchman",10.0));
+        mangas.add(new Manga(9L,"Dandadan",15.0));
+        mangas.add(new Manga(2L,"Mob psycho 100",10.0));
+        mangas.add(new Manga(11L,"One piece",20.9));
+        for (Manga manga: mangas){
+            System.out.println(manga);
+        }
     }
 }
