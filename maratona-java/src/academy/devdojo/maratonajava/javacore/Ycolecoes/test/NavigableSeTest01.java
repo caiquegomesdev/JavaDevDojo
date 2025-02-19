8e2ac5a7e06d0d0595ea4647c3cfe73b0d02d2d7
Caique Gomes <caiquegomesdev@gmail.com>
2025-02-18 22:31:16 -0300

    Aula177 - Coleções pt 17 - NavigableSet, TreeSet pt 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSeTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSeTest01.java
@@ -3,15 +3,28 @@ package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
 import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Smartphone;
 
+import java.util.Comparator;
 import java.util.NavigableSet;
 import java.util.TreeSet;
+
+class SmartphoneMarcaComparator implements Comparator<Smartphone>{
+    @Override
+    public int compare(Smartphone o1, Smartphone o2) {return o1.getMarca().compareTo(o2.getMarca());}
+}
+
+class MangaPrecoComparator implements  Comparator<Manga>{
+    @Override
+    public int compare(Manga o1, Manga o2){
+        return Double.compare(o1.getPreco(), o2.getPreco());
+    }
+}
 
 public class NavigableSeTest01 {
     public static void main(String[] args) {
         NavigableSet<Smartphone> set = new TreeSet<>();
         Smartphone smartphone = new Smartphone("123", "Nokia");
         set.add(smartphone);
-        NavigableSet<Manga> mangas = new TreeSet<>();
+        NavigableSet<Manga> mangas = new TreeSet<>(new MangaPrecoComparator());
         mangas.add(new Manga(5L,"Attack on titan",19.9));
         mangas.add(new Manga(3L,"Villand Saga",9.5));
         mangas.add(new Manga(1L,"Berserk",29.9));
@@ -23,8 +36,26 @@ public class NavigableSeTest01 {
         mangas.add(new Manga(9L,"Dandadan",15.0));
         mangas.add(new Manga(2L,"Mob psycho 100",10.0));
         mangas.add(new Manga(11L,"One piece",20.9));
-        for (Manga manga: mangas.descendingSet()){
+        for (Manga manga: mangas){
             System.out.println(manga);
         }
+
+        Manga yuyu = new Manga(21,"YUYU HAKUSHO",8,5);
+
+        //lower <
+        //floor <=
+        //higher >
+        //ceiling >=
+
+        System.out.println("----------------------");
+        System.out.println(mangas.lower(yuyu));
+        System.out.println(mangas.floor(yuyu));
+        System.out.println(mangas.higher(yuyu));
+        System.out.println(mangas.ceiling(yuyu));
+
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollFirst());
+        System.out.println(mangas.pollLast());
+        System.out.println(mangas.size());
     }
 }
