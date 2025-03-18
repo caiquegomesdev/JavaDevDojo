4523f714a442e9bc5791e08a922fb618953e958a
Caique Gomes <caiquegomesdev@gmail.com>
2025-03-17 22:43:57 -0300

    Aula179 - Coleções pt 19 - Map, HashMap, LinkedHashMap pt 02

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest02.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Consumidor;
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+public class MapTest02 {
+    public static void main(String[] args) {
+        Consumidor consumidor1 = new Consumidor("Caique Gomes");
+        Consumidor consumidor2 = new Consumidor("DEVGOMES");
+
+        Manga manga1 = new Manga(5L, "Attack on titan", 19.9);
+        Manga manga2 =new Manga(3L,"Villand Saga",9.5);
+        Manga manga3 =new Manga(1L,"Berserk",29.9);
+        Manga manga4 =new Manga(4L,"Ousama Ranking",18.9);
+        Manga manga5 =new Manga(6L,"Hunte X Hunter",20.0);
+        Manga manga6 =new Manga(11L,"One piece",20.9);
+
+        
+    }
+}
