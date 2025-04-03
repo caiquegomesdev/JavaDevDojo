c7b8f0bd6ffdc88dec15d47a0eebca1b1b9c138e
Caique Gomes <caiquegomesdev@gmail.com>
2025-04-02 23:34:28 -0300

    Aula183 - Generics pt 01 - Introdução

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/GenericsTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/GenericsTest01.java
@@ -7,23 +7,14 @@ import java.util.List;
 
 public class GenericsTest01 {
     public static void main(String[] args) {
-        List lista = new ArrayList();
+        //Type erasure
+        List<String > lista = new ArrayList<>();
         lista.add("Luffy");
-        lista.add(123L);
-        lista.add(new Consumidor("Goku"));
+        lista.add("Luffy");
 
-        for (Object o : lista) {
-            if(o instanceof String){
-                System.out.println(o);
-            }
-            if (o instanceof Long){
-                System.out.println(o);
-            }
-            if (o instanceof Consumidor){
-                Consumidor c = (Consumidor) o;
-                System.out.println(c);
-            }
 
+        for (String o : lista) {
+            System.out.println(o);
         }
 
     }
