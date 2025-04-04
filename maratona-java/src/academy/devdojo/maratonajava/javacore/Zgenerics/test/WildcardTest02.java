ac3f68cd374659df8533527ee05b2b83abcd2a45
Caique Gomes <caiquegomesdev@gmail.com>
2025-04-03 22:26:58 -0300

    Aula184 - Generics pt 02 - Wildcard pt 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
@@ -7,10 +7,14 @@ public class WildcardTest02 {
     public static void main(String[] args) {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
+
     }
+    //type erasure
     private static void printConsulta(List<Animal> animals){
         for (Animal animal : animals){
             animal.consulta();
         }
+        animals.add(new Cachorro());
+        animals.add(new Gato());
     }
 }
