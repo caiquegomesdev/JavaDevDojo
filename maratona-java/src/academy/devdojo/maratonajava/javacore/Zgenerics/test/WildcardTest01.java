626a40b6ffc4d8bf582eb25d856dd35a4eb62fb0
Caique Gomes <caiquegomesdev@gmail.com>
2025-04-03 22:06:41 -0300

    Aula184 - Generics pt 02 - Wildcard pt 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest01.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+
+abstract class Animal{
+    public abstract void consulta();
+}
+class  Cachorro extends Animal{
+    @Override
+    public void consulta(){
+        System.out.println("Consultando Dog");
+    }
+}
+class Gato extends Animal{
+    @Override
+    public void consulta(){
+        System.out.println("Consultando Cat");
+    }
+}
+public class WildcardTest01 {
+    public static void main(String[] args) {
+        Cachorro[] cachorros = {new Cachorro(), new Cachorro()};
+        Gato[] gatos = {new Gato(), new Gato()};
+        printConsulta(cachorros);
+        printConsulta(gatos);
+    }
+    private static void printConsulta(Animal[] animals){
+        for (Animal animal : animals){
+            animal.consulta();
+        }
+    }
+}
