7433bf18efc6b90c7d5a1b87e5b64fb61148fabe
Caique Gomes <caiquegomesdev@gmail.com>
2025-04-07 23:11:06 -0300

    Aula186 - Generics pt 04 - Classes Genéricas pt 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/ClassGenericaTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/ClassGenericaTest01.java
@@ -1,4 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Zgenerics.test;
 
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Zgenerics.service.CarroRentavelService;
+
 public class ClassGenericaTest01 {
+    public static void main(String[] args) {
+        CarroRentavelService carroRentavelService = new CarroRentavelService();
+        Carro carro = carroRentavelService.buscarCarroDisponivel();
+        System.out.println("Usando carro por um mes...");
+        carroRentavelService.retornarCarroAlugado(carro);
+    }
 }
