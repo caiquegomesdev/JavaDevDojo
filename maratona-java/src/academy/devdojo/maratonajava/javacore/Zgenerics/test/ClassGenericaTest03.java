984c52defc5bab8cca8ceb739a4f54faca730c1b
Caique Gomes <caiquegomesdev@gmail.com>
2025-04-08 23:51:19 -0300

    Aula187 - Generics pt 05 - Classes Genéricas pt 02

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/ClassGenericaTest03.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Barco;
+import academy.devdojo.maratonajava.javacore.Zgenerics.service.BarcoRentavelService;
+
+public class ClassGenericaTest03 {
+    public static void main(String[] args) {
+
+    }
+}
