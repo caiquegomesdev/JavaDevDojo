7f694ab27ff24349b88520eb556d307e1ec904ba
Caique Gomes <caiquegomesdev@gmail.com>
2025-04-09 22:07:20 -0300

    Aula188 - Generics pt 06 - Métodos Genéricos

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/MetodoGenericoTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Barco;
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Carro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MetodoGenericoTest01 {
+    public static void main(String[] args) {
+        criarArrayComUmObjeto(new Barco("Canoa Marota"));
+    }
+
+    private static <T> void criarArrayComUmObjeto(T t){
+        List<T> list = List.of(t);
+        System.out.println(list);
+    }
+}
