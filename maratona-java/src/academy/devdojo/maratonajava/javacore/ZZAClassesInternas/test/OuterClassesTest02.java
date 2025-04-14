3010ae0d9fa6d559d69e0ad97d8e466064b9fd2e
Caique Gomes <caiquegomesdev@gmail.com>
2025-04-13 22:43:32 -0300

    Aula190 - Classes Internas pt 02 - Classes Locais

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZAClassesInternas/test/OuterClassesTest02.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZAClassesInternas/test/OuterClassesTest02.java
@@ -4,9 +4,11 @@ public class OuterClassesTest02 {
     private String nome = "Midoriya";
 
     void print(){
+        String lastName = "Izuku";
+        
         class LocalClass{
             public  void printLocal(){
-                System.out.println(nome);
+                System.out.println(nome + " " +lastName);
             }
         }
         new LocalClass().printLocal();
