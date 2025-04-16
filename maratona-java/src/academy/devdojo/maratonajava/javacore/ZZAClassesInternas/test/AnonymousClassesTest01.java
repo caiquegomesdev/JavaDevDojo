8d5637193e54db9860cecc4ae337b2c95849b460
Caique Gomes <caiquegomesdev@gmail.com>
2025-04-15 22:20:08 -0300

    Aula191 - Classes Internas pt 03 - Classes Anônimas

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZAClassesInternas/test/AnonymousClassesTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZAClassesInternas/test/AnonymousClassesTest01.java
@@ -6,15 +6,15 @@ class Animal{
     }
 }
 
-class Cachorro extends Animal{
-    @Override
-    public void walk() {
-        System.out.println("Cachorro walking");
-    }
-}
 
 public class AnonymousClassesTest01 {
     public static void main(String[] args) {
-
+        Animal animal = new Animal(){
+            @Override
+            public void walk() {
+                System.out.println("Walking in the shadows");
+            }
+        };
+        animal.walk();
     }
 }
