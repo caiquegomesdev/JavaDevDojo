f82836bc0d88b1e9c10c95faf488863ef72f4fc8
Caique Gomes <caiquegomesdev@gmail.com>
2025-05-01 00:02:12 -0300

    Aula194 - Parametrizando comportamentos pt 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/test/ComportamentoPorParametroTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/test/ComportamentoPorParametroTest01.java
@@ -15,13 +15,21 @@ public class ComportamentoPorParametroTest01 {
     );
 
     public static void main(String[] args) {
-        List<Car> greenCars = filter(cars, new CarPredicate() {
-            @Override
-            public boolean test(Car car) {
-                return car.getColor().equals("green");
-            }
-        });
+//        List<Car> greenCars = filter(cars, new CarPredicate() {
+//            @Override
+//            public boolean test(Car car) {
+//                return car.getColor().equals("green");
+//            }
+//        });
+
+        List<Car> greenCars = filter(cars, car -> car.getColor().equals("green"));
+        List<Car> redCars = filter(cars, car -> car.getColor().equals("red"));
+        List<Car> blackCars = filter(cars, car -> car.getColor().equals("black"));
+        List<Car> yearBeforeCars = filter(cars, car -> car.getYear() < 2015);
         System.out.println(greenCars);
+        System.out.println(redCars);
+        System.out.println(blackCars);
+        System.out.println(yearBeforeCars);
     }
 
     private static List<Car> filter(List<Car> cars, CarPredicate carPredicate){
