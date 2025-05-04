f1d061dfc699e3df0d01efeb26ab67024833e022
Caique Gomes <caiquegomesdev@gmail.com>
2025-05-04 12:01:27 -0300

    Aula199 - Method Reference pt 02 - Referência a métodos não estáticos

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest03.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest03.java
@@ -2,12 +2,16 @@ package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class MethodReferenceTest03 {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>(List.of("Rimuru", "Veldora", "Hikimaru"));
         list.sort(String::compareTo);
         System.out.println(list);
-        
+
+        Function<String, Integer> numStringToInterger = Integer :: parseInt;
+        Integer num = numStringToInterger.apply("10");
+        System.out.println(num);
     }
 }
