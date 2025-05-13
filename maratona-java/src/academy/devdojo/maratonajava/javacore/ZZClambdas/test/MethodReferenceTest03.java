d2ab4a47c96a717b73ae45bdcde595ca17d120a4
Caique <caiquegomesdev@gmail.com>
2025-05-12 17:49:32 -0700

    Aula199 - Method Reference pt 02 - Referência a métodos não estáticos

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest03.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest03.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiPredicate;
 import java.util.function.Function;
 
 public class MethodReferenceTest03 {
@@ -13,5 +14,8 @@ public class MethodReferenceTest03 {
         Function<String, Integer> numStringToInterger = Integer :: parseInt;
         Integer num = numStringToInterger.apply("10");
         System.out.println(num);
+
+        BiPredicate<List<String>,String> checkName = List::contains;
+        System.out.println(checkName.test(list,"Rimuru"));
     }
 }
